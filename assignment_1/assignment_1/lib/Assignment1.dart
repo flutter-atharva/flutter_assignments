f32528be90b2f4cdfcf3aecf4b3fdeff35f44a7a
flutter-atharva <atharvaflutter123@gmail.com>
2024-03-04 20:40:44 +0530

    updated

---
+++ assignment_1/assignment_1/lib/Assignment1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatelessWidget {
+  const Assignment1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Assignment1"),
+      ),
+      body:const Center(
+        child: Text("Core2Web"),
+      ),
+    
+    );
+  }
+}
